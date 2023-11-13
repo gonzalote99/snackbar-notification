97167cede8a7f1f82347a9d21d7b54f30a089311
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-11-13 15:08:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# snackbar-notification
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/gonzalote99/snackbar-notification)
---
+++ script.js
@@ -0,0 +1,39 @@
+var btn = document.querySelector('.btn');
+var bar = document.querySelector('.snackbar-wrapper');
+
+btn.addEventListener('click', () => {
+  btn.disabled = true;
+
+  showPromise().then(setTimeout(() => {
+    hidePromise().then(() => {
+      btn.disabled = false;
+    });;
+  }, 3000));
+
+});
+
+var showPromise = () => {
+  return new Promise((resolve, reject) => {
+    bar.style.display = 'inline-block';
+   setTimeout(() => {
+     bar.style.opacity = 1;
+     bar.style.top = "90vh";
+     resolve();
+   }, 10);
+
+  });
+}
+
+var hidePromise = () => {
+  return new Promise((resolve, reject) => {
+    bar.style.opacity = 0;
+    bar.style.top = "110vh";
+
+    setTimeout(() => {
+      bar.style.display = "none";
+      resolve();
+
+    }, 500);
+
+  });
+}
